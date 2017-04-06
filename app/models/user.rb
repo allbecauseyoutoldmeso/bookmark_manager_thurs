a0c30df33f362f013aa8b8d8dd47058d680cfbc8
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-06 21:33:10 +0100

    Error messages displayed better

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User
   has n, :links, through: Resource
 
   property :id, Serial
-  property :email, String, required: true #this tells the database not to accept users sans emails
+  property :email, String # , required: true #this tells the database not to accept users sans emails
   property :password_digest, Text
   attr_reader :password
   attr_accessor :confirm_password
@@ -22,12 +22,15 @@ class User
 
   # default would be password_confirmation, unless changed like below
   validates_confirmation_of :password, :confirm => :confirm_password,
-    :message => "Your passwords don't match" 
-  # validates_presence_of :email   ...this prevents creation of user without email, but is unnecessary if use required: true as above
-  validates_format_of :email, as: :email_address
+    :message => "Doh.  Your passwords don't match."
+  validates_presence_of :email,
+    :message => "Really?  Dude!  Please enter an email address in the email field."
+  # ...this prevents creation of user without email, but is unnecessary if use 'required: true' next to property :email aboveove
+  validates_format_of :email, as: :email_address,
+    :message =>  "Please enter a valid email address."
   #or next to property :email can put format: :email_address
   validates_uniqueness_of :email,
-    :message => "This email address is already registered"
+    :message => "Doh.  This email address is already registered."
 
   #or next to email unique: true
 end
